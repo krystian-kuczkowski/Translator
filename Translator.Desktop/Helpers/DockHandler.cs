a4353a63a8df04fb95df4e7b6234eff1a14326cd
Krystian Kuczkowski <krystian.kuczkowski@outlook.com>
2019-04-24 19:07:51 +0200

    Added a class for window docking handling

---
+++ Translator.Desktop/Helpers/DockHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Translator.Desktop.Helpers
+{
+    public class DockHandler
+    {
+        private readonly Window _window;
+
+        public DockHandler(Window window)
+        {
+            this._window = window;
+        }
+
+        public bool IsWindowDocked()
+        {
+            return _window.Top == 0 && (_window.Top + _window.Height) >= _window.Height;
+        }
+    }
+}
