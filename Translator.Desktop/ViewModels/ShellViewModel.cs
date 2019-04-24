8180ba5498d66bcd9c91374cae0166a38aecb4b4
Krystian Kuczkowski <krystian.kuczkowski@outlook.com>
2019-04-24 19:12:27 +0200

    Extended main view model

--- Translator.Desktop/ViewModels/ShellViewModel.cs
+++ Translator.Desktop/ViewModels/ShellViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,79 +14,125 @@ namespace Translator.Desktop.ViewModels
 {
     public class ShellViewModel : BaseViewModel
     {
-        private WindowState _windowState;
         private readonly int _outerMarginSize = 10;
         private readonly int _windowRadius = 0;
-        private WindowDockPosition dockPosition = WindowDockPosition.Undocked;
+        private readonly int _contentPadding = 10;
 
-        public double WindowMinimumWidth {
+        private WindowState _windowState;
+        private DockHandler _dockHandler;
+
+        public double WindowMinimumWidth
+        {
             get => 450;
         }
 
-        public double WindowMinimumHeight {
+        public double WindowMinimumHeight
+        {
             get => 300;
         }
 
-        public int ResizeBorder {
+        public int ResizeBorder
+        {
             get => 6;
         }
 
-        public int TitleBarHeight {
+        public int TitleBarHeight
+        {
             get => 24;
         }
 
-        public bool Borderless {
-            get => _windowState == WindowState.Maximized || dockPosition != WindowDockPosition.Undocked;
+        public Thickness MenuBarPadding
+        {
+            get => new Thickness(0, TitleBarHeightGridLength.Value, 0, 0);
         }
 
-        public GridLength TitleBarHeightGridLength {
+        public bool Borderless
+        {
+            get
+            {
+                if (_windowState != WindowState.Maximized && _dockHandler == null)
+                    return false;
+
+                return _windowState == WindowState.Maximized || _dockHandler.IsWindowDocked();
+            }
+        }
+
+        public GridLength TitleBarHeightGridLength
+        {
             get => new GridLength(TitleBarHeight + ResizeBorder);
         }
 
-        public Thickness ResizeBorderThickness {
+        public Thickness ResizeBorderThickness
+        {
             get => new Thickness(ResizeBorder + OuterMarginSize);
         }
 
-        public Thickness InnerContentPadding {
-            get => new Thickness(ResizeBorder);
+        public Thickness InnerContentPadding
+        {
+            get => new Thickness(_contentPadding);
         }
 
-        public Thickness OuterMarginSizeThickness {
+        public Thickness OuterMarginSizeThickness
+        {
             get => new Thickness(OuterMarginSize);
         }
 
-        public CornerRadius WindowCornerRadius {
+        public CornerRadius WindowCornerRadius
+        {
             get => new CornerRadius(WindowRadius);
         }
 
-        public int OuterMarginSize {
+        public int OuterMarginSize
+        {
             get => Borderless ? 0 : _outerMarginSize;
         }
 
-        public int WindowRadius {
+        public int WindowRadius
+        {
             get => Borderless ? 0 : _windowRadius;
         }
 
-        public ICommand StateChangedCommand { get; private set; }
         public ICommand WindowLoadedCommand { get; private set; }
+        public ICommand StateChangedCommand { get; private set; }
+        public ICommand SizeChangedCommand { get; private set; }
         public ICommand WindowMinimizeCommand { get; private set; }
         public ICommand WindowMaximizeCommand { get; private set; }
         public ICommand WindowCloseCommand { get; private set; }
+        public ICommand TranslationsViewCommand { get; private set; }
+        public ICommand FilesViewCommand { get; private set; }
+        public ICommand SettingsViewCommand { get; private set; }
 
-        public ShellViewModel()
+        private readonly TranslationsViewModel _translationsViewModel;
+        private readonly FilesViewModel _filesViewModel;
+        private readonly SettingsViewModel _settingsViewModel;
+
+        public BaseViewModel ViewModel { get; set; }
+
+        public ShellViewModel(TranslationsViewModel translationsViewModel, FilesViewModel filesViewModel, SettingsViewModel settingsViewModel)
         {
-            StateChangedCommand = new RelayCommand(param => {
-                _windowState = (WindowState)param;
+            InitCommands();
+
+            _translationsViewModel = translationsViewModel;
+            _filesViewModel = filesViewModel;
+            _settingsViewModel = settingsViewModel;
+
+            ViewModel = _translationsViewModel;
+        }
+
+        private void InitCommands()
+        {
+            WindowLoadedCommand = new RelayCommand(param => {
+                _windowState = ((Window)param).WindowState;
+                _dockHandler = new DockHandler((Window)param);
                 OnWindowResized();
             });
 
-            WindowLoadedCommand = new RelayCommand(param => {
+            StateChangedCommand = new RelayCommand(param => {
                 _windowState = ((Window)param).WindowState;
-                var resizer = new WindowResizer((Window)param);
-                resizer.WindowDockChanged += (dock) => {
-                    dockPosition = dock;
-                    OnWindowResized();
-                };
+                OnWindowResized();
+            });
+
+            SizeChangedCommand = new RelayCommand(param => {
                 OnWindowResized();
             });
 
@@ -103,6 +150,21 @@ namespace Translator.Desktop.ViewModels
 
             WindowCloseCommand = new RelayCommand(param => {
                 ((Window)param).Close();
+            });
+
+            TranslationsViewCommand = new RelayCommand(param => {
+                ViewModel = _translationsViewModel;
+                OnPropertyChanged(nameof(ViewModel));
+            });
+
+            FilesViewCommand = new RelayCommand(param => {
+                ViewModel = _filesViewModel;
+                OnPropertyChanged(nameof(ViewModel));
+            });
+
+            SettingsViewCommand = new RelayCommand(param => {
+                ViewModel = _settingsViewModel;
+                OnPropertyChanged(nameof(ViewModel));
             });
         }
 
