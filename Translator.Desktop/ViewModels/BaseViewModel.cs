1e8fa79e0cf673a8446565527ded75ebc012d77c
Krystian Kuczkowski <krystian.kuczkowski@outlook.com>
2019-04-27 13:48:37 +0200

    Added grid content length

--- Translator.Desktop/ViewModels/BaseViewModel.cs
+++ Translator.Desktop/ViewModels/BaseViewModel.cs
@@ -4,11 +4,22 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Translator.Desktop.ViewModels
 {
     public abstract class BaseViewModel : INotifyPropertyChanged
     {
+        private double _gridContentLength = 300;
+
+        public GridLength GridContentLength
+        {
+            get
+            {
+                return new GridLength(_gridContentLength);
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void OnPropertyChanged(string propertyName)
