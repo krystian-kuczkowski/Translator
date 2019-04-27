f179836a1b336073608339f19f43281cf24d555e
Krystian Kuczkowski <krystian.kuczkowski@outlook.com>
2019-04-27 22:13:29 +0200

    Added a body of the class

--- Translator.Desktop/ViewModels/TranslationsViewModel.cs
+++ Translator.Desktop/ViewModels/TranslationsViewModel.cs
@@ -4,11 +4,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Translator.Application.Business;
+using Translator.Application.Helpers;
+using Translator.Desktop.Commands;
 
 namespace Translator.Desktop.ViewModels
 {
     public class TranslationsViewModel : BaseViewModel
     {
+        private readonly TranslationsProvider _translationsProvider;
+        private readonly ILanguageSettings _languageSettings;
 
+        public string SourceLanguage {
+            get {
+                return _languageSettings.SourceLanguage.ToString();
+            }
+        }
+
+        public string TargetLanguage {
+            get {
+                return _languageSettings.TargetLanguage.ToString();
+            }
+        }
+
+        public string SourceText { get; set; }
+        public string TargetText { get; set; }
+
+        public ICommand TranslateCommand { get; private set; }
+
+        public TranslationsViewModel(TranslationsProvider translationsProvider, ILanguageSettings languageSettings)
+        {
+            _translationsProvider = translationsProvider;
+            _languageSettings = languageSettings;
+
+            TranslateCommand = new RelayCommand(param => {
+                Translate();
+            }, param => !String.IsNullOrWhiteSpace(SourceText));
+        }
+
+        private void Translate()
+        {
+            TargetText = _translationsProvider.TranslateAsync(SourceText).GetAwaiter().GetResult();
+            OnPropertyChanged(nameof(TargetText));
+        }
     }
 }
